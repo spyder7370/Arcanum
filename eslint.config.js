390f3dfa5f1a050bf341f058e53633e205295a4e
sp <hiteshy9904@gmail.com>
2025-05-08 16:47:40 +0530

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
@@ -7,18 +8,19 @@ import svelteConfig from './svelte.config.js';
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...svelte.configs.recommended,
+	prettier,
+	...svelte.configs.prettier,
 	{
 		languageOptions: {
-			globals: { ...globals.browser, ...globals.node }
-		}
+			globals: { ...globals.browser, ...globals.node },
+		},
 	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.js'],
-		languageOptions: { parserOptions: { svelteConfig } }
-	}
+		languageOptions: { parserOptions: { svelteConfig } },
+	},
 ];
